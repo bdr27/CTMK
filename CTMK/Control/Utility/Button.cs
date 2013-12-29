211edffd09532f6a4378120edce7efde9307888a
Brendan Russo <brendanrusso27@gmail.com>
2013-12-29 13:58:28 +1000

    Moving Code
    Started moving old code to this new solutions and repo

---
+++ CTMK/Control/Utility/Button.cs
@@ -0,0 +1,48 @@
+﻿
+namespace CTMK.Control.Utility
+{
+    public class Button
+    {
+
+        private string name;
+        private bool down = false;
+        private bool released = false;
+
+        public Button(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void ButtonDown()
+        {
+            released = false;
+            down = true;
+        }
+
+        public void ButtonUp()
+        {
+            down = false;
+            released = true;
+        }
+
+        public bool GetDown()
+        {
+            return down;
+        }
+
+        public bool GetReleased()
+        {
+            if (released)
+            {
+                released = false;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CTMK/Control/Utility/ThumbstickState.cs
@@ -0,0 +1,43 @@
+﻿using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTMK.Control.Utility
+{
+    public class ThumbstickState
+    {
+        private readonly string name;
+        private readonly Vector2 position;
+        private readonly bool down;
+
+        public ThumbstickState(string name, Vector2 position, bool down)
+        {
+            this.name = name;
+            this.down = down;
+            this.position = position;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public float GetX()
+        {
+            return position.X;
+        }
+
+        public float GetY()
+        {
+            return position.Y;
+        }
+
+        public bool GetClicked()
+        {
+            return down;
+        }
+    }
+}
